249bbfe8cf89c620d7b63f4dd5e06633b7790c5a
JeremyCPE <mohariry.jeremy@gmail.com>
2024-05-29 17:43:07 +0900

    Ajoutez des fichiers projet.

---
+++ Luxor/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Luxor.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ Luxor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Luxor.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
