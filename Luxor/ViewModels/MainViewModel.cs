6d08c9b2be3ac85b144e09f0476f90dea717d1e2
JeremyCPE <mohariry.jeremy@gmail.com>
2024-06-01 01:50:57 +0900

    (feat) add empty Settings page

--- Luxor/ViewModels/MainViewModel.cs
+++ Luxor/ViewModels/MainViewModel.cs
@@ -1,6 +1,25 @@
-﻿namespace Luxor.ViewModels;
+﻿using System.Windows.Input;
+using ReactiveUI;
+
+namespace Luxor.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    bool _switchBrightness = false;
+    public ICommand BrightnessCommand { get; }
+    public ICommand SettingsCommand { get; }
+
+
+
+    public MainViewModel() 
+    {
+        BrightnessCommand = ReactiveCommand.Create(() =>
+        {
+            _switchBrightness = !_switchBrightness;
+        });
+
+        SettingsCommand = ReactiveCommand.Create(() =>
+        {
+        });
+    }
 }
---
+++ Luxor/Views/Settings.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Luxor.Views
+{
+    public partial class Settings : Window
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+    }
+}
