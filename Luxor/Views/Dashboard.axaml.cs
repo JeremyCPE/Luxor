0ddd8e91223176f8e00fa0e3fd850ce498411bdf
JeremyCPE <mohariry.jeremy@gmail.com>
2024-06-05 00:18:30 +0900

    (feat) add dashboard

---
+++ Luxor/Views/Dashboard.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Luxor.Views
+{
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
